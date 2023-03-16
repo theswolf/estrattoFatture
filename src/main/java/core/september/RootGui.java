016f39c1baece54aaa412eb89a51d86d07dce1dd
Christian Geymonat <chris.geymo@gmail.com>
2023-03-16 11:37:06 +0100

    runnable jar

--- src/main/java/core/september/RootGui.java
+++ src/main/java/core/september/RootGui.java
@@ -1,8 +1,9 @@
 package core.september;
 
 import com.formdev.flatlaf.FlatLightLaf;
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.StringStack;
-import com.sun.xml.internal.messaging.saaj.packaging.mime.util.QPDecoderStream;
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 import core.september.menu.MenuHandler;
 import core.september.util.PDFData;
 import core.september.util.PDFRunnable;
@@ -10,7 +11,6 @@ import core.september.util.PdfHandler;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.event.*;
@@ -31,9 +31,9 @@ public class RootGui extends JFrame {
     private File file;
 
     public RootGui() {
+        $$$setupUI$$$();
         setContentPane(contentPane);
         getRootPane().setDefaultButton(buttonOK);
-
 
 
         buttonOK.addActionListener(new ActionListener() {
@@ -63,8 +63,8 @@ public class RootGui extends JFrame {
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
-        new MenuHandler<>(this,(option,filechooser) -> {
-            if(option != JFileChooser.CANCEL_OPTION) {
+        new MenuHandler<>(this, (option, filechooser) -> {
+            if (option != JFileChooser.CANCEL_OPTION) {
                 fileList.setModel(new DefaultListModel());
                 file = filechooser.getSelectedFile();
                 selectedFileLabel.setText(file.getAbsolutePath());
@@ -80,8 +80,8 @@ public class RootGui extends JFrame {
         });
 
         readButton.addActionListener((a) -> {
-            if(file != null) {
-                PDFRunnable pdfRunnable = new PDFRunnable(file,tableResult);
+            if (file != null) {
+                PDFRunnable pdfRunnable = new PDFRunnable(file, tableResult);
                 Thread t = new Thread(pdfRunnable);
                 t.start();
 
@@ -98,9 +98,9 @@ public class RootGui extends JFrame {
                 JFileChooser fileChooser = new JFileChooser();
                 fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
                 int option = fileChooser.showOpenDialog(RootGui.this);
-                if(option != JFileChooser.CANCEL_OPTION) {
+                if (option != JFileChooser.CANCEL_OPTION) {
                     File dest = fileChooser.getSelectedFile();
-                    PdfHandler pdfHandler = new PdfHandler(dest.getAbsolutePath(), tableResult.getModel(),PDFData.columns);
+                    PdfHandler pdfHandler = new PdfHandler(dest.getAbsolutePath(), tableResult.getModel(), PDFData.columns);
                     new Thread(pdfHandler).start();
                 }
             }
@@ -113,14 +113,13 @@ public class RootGui extends JFrame {
     }
 
 
-
     public static void main(String[] args) {
         /* Use an appropriate Look and Feel */
         try {
             System.setProperty("apple.laf.useScreenMenuBar", "true");
             System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Fatture");
 
-            UIManager.setLookAndFeel( new FlatLightLaf() );
+            UIManager.setLookAndFeel(new FlatLightLaf());
             //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -129,7 +128,7 @@ public class RootGui extends JFrame {
 
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 RootGui dialog = new RootGui();
                 dialog.pack();
@@ -140,22 +139,22 @@ public class RootGui extends JFrame {
 
 
     private void createUIComponents() {
-        tableResult = new JTable(PDFData.emptySet(PDFData.columns),PDFData.columns);
-        tableResult.setDefaultRenderer(Object.class, new TableCellRenderer(){
-            private DefaultTableCellRenderer DEFAULT_RENDERER =  new DefaultTableCellRenderer();
+        tableResult = new JTable(PDFData.emptySet(PDFData.columns), PDFData.columns);
+        tableResult.setDefaultRenderer(Object.class, new TableCellRenderer() {
+            private DefaultTableCellRenderer DEFAULT_RENDERER = new DefaultTableCellRenderer();
+
             @Override
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
                 Component c = DEFAULT_RENDERER.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
 
-                if (row%2 == 0){
+                if (row % 2 == 0) {
                     c.setBackground(Color.WHITE);
-                }
-                else {
+                } else {
                     c.setBackground(Color.LIGHT_GRAY);
                 }
 
-                if(row == table.getRowCount() -1 ) {
+                if (row == table.getRowCount() - 1) {
                     c.setBackground(Color.YELLOW);
                 }
 
@@ -164,5 +163,77 @@ public class RootGui extends JFrame {
             }
 
         });
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        createUIComponents();
+        contentPane = new JPanel();
+        contentPane.setLayout(new GridLayoutManager(3, 1, new Insets(10, 10, 10, 10), -1, -1));
+        contentPane.setAlignmentY(0.5f);
+        contentPane.setAutoscrolls(false);
+        contentPane.setPreferredSize(new Dimension(800, 600));
+        contentPane.setRequestFocusEnabled(true);
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
+        contentPane.add(panel1, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel1.add(spacer1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1, true, false));
+        panel1.add(panel2, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        buttonOK = new JButton();
+        buttonOK.setText("OK");
+        panel2.add(buttonOK, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        buttonCancel = new JButton();
+        buttonCancel.setText("Cancel");
+        panel2.add(buttonCancel, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), -1, -1));
+        contentPane.add(panel3, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_NORTH, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        selectedFileLabel = new JLabel();
+        selectedFileLabel.setText("No file selected");
+        panel3.add(selectedFileLabel, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel4 = new JPanel();
+        panel4.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
+        panel3.add(panel4, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        readButton = new JButton();
+        readButton.setText("Read");
+        panel4.add(readButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panel4.add(spacer2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final JPanel panel5 = new JPanel();
+        panel5.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel5.setAutoscrolls(false);
+        panel3.add(panel5, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(-1, 200), new Dimension(-1, 200), 0, false));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        panel5.add(scrollPane1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        fileList = new JList();
+        fileList.setAutoscrolls(true);
+        final DefaultListModel defaultListModel1 = new DefaultListModel();
+        fileList.setModel(defaultListModel1);
+        fileList.setName("1");
+        scrollPane1.setViewportView(fileList);
+        final JPanel panel6 = new JPanel();
+        panel6.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        contentPane.add(panel6, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final JScrollPane scrollPane2 = new JScrollPane();
+        panel6.add(scrollPane2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        tableResult.setCellSelectionEnabled(false);
+        tableResult.setRequestFocusEnabled(true);
+        scrollPane2.setViewportView(tableResult);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return contentPane;
     }
 }
